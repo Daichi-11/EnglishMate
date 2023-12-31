950434c9ae4856b392f1d321ddfcf06d0195f0f7
Daichi-11 <dai77yama@gmail.com>
2023-12-31 20:57:08 +0900

    fix: navbar design

--- englishmate/flashcards/models.py
+++ englishmate/flashcards/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 
 class FlashCard(models.Model):
-    front_image = models.ImageField(upload_to='flashcards/')
-    front_definition = models.CharField(max_length=255)
-    back_image = models.ImageField(upload_to='flashcards/')
-    back_word = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='flashcards/')
+    definition = models.CharField(max_length=255)
+    word = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.front_definition} - {self.back_word}"
+        return f"{self.definition} - {self.word}"
