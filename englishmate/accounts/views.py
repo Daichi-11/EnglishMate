76bfca094e4119734b844fa5c72eef21eb07adaf
Daichi-11 <dai77yama@gmail.com>
2024-01-03 13:20:41 +0900

    add: profile

--- englishmate/accounts/views.py
+++ englishmate/accounts/views.py
@@ -1,10 +1,11 @@
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UserProfileForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
-
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 
 def user_signup(request):
     if request.method == 'POST':
@@ -32,7 +33,49 @@ def user_login(request):
 
     return render(request, 'registration/login.html', {'form': form})
 
+@login_required
 def user_logout(request):
     logout(request)
     messages.success(request, 'You have successfully logged out.')
     return redirect('home') 
+
+
+
+@login_required
+def user_profile(request):
+    return render(request, 'registration/profile.html')
+
+@login_required
+def edit_profile(request):
+    try:
+        user_profile = request.user.userprofile
+    except UserProfile.DoesNotExist:
+        user_profile = UserProfile(user=request.user)
+        user_profile.save()
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully!')
+            return redirect('registration:profile')
+    else:
+        form = UserProfileForm(instance=user_profile)
+
+    return render(request, 'registration/edit_profile.html', {'form': form})
+
+@login_required
+def delete_profile(request):
+    try:
+        user_profile = request.user.userprofile
+    except UserProfile.DoesNotExist:
+        user_profile = None
+
+    if request.method == 'POST':
+        if user_profile:
+            user_profile.delete()
+            request.user.delete()
+            logout(request)
+            return redirect('registration:login')
+
+    return render(request, 'registration/delete_profile.html')
