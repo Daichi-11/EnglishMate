b145d238af8060009c0f8b83233ee3f6a80a5360
Daichi-11 <dai77yama@gmail.com>
2024-01-04 13:51:27 +0900

    add: dictionary function

--- englishmate/flashcards/urls.py
+++ englishmate/flashcards/urls.py
@@ -1,6 +1,6 @@
 # flashcards/urls.py
 from django.urls import path
-from .views import flashcard_list, flashcard_detail, flashcard_create, flashcard_edit, flashcard_delete
+from .views import flashcard_list, flashcard_detail, flashcard_create, flashcard_edit, flashcard_delete, dictionary
 
 urlpatterns = [
     path('', flashcard_list, name='flashcard_list'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('create/', flashcard_create, name='flashcard_create'),
     path('<int:pk>/edit/', flashcard_edit, name='flashcard_edit'),
     path('<int:pk>/delete/', flashcard_delete, name='flashcard_delete'),
+    path('dictionary/', dictionary, name='dictionary'),
 ]
